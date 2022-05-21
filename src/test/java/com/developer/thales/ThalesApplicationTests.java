7bbcb685a09e47436680f11dc070670cccecbacd
DiegoMurcia2022 <diegomurciac2220@gmail.com>
2022-05-20 19:53:34 -0500

    test for annual salary

--- src/test/java/com/developer/thales/ThalesApplicationTests.java
+++ src/test/java/com/developer/thales/ThalesApplicationTests.java
@@ -1,13 +1,24 @@
 package com.developer.thales;
 
+import com.developer.thales.models.Employee;
+import com.developer.thales.services.EmployeeService;
+import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 @SpringBootTest
 class ThalesApplicationTests {
+	@Autowired
+	EmployeeService employeeService;
 
 	@Test
-	void contextLoads() {
+	public void testAnnualSalaryCalculation() throws IOException, ParseException {
+		long result = 3849600;
+		Employee employee = employeeService.getEmployee(1);
+		Assertions.assertEquals(result, employee.getAnnualSalary());
 	}
-
 }
